c0be9c4b1a47702541985aaa04189fe881f3403f
mr. noone <smth@smth.com>
2016-06-25 22:46:43 +0300

    Simple Face Detector, using OpenCV

--- WebcamPanel.java
+++ WebcamPanel.java
@@ -1,16 +1,20 @@
 package com.company;
 
 import org.bytedeco.javacpp.opencv_core;
-import org.bytedeco.javacv.CanvasFrame;
-import org.bytedeco.javacv.FrameGrabber;
-import org.bytedeco.javacv.OpenCVFrameConverter;
-import org.bytedeco.javacv.OpenCVFrameGrabber;
+import org.bytedeco.javacpp.opencv_videoio;
+import org.bytedeco.javacpp.presets.avutil;
+import org.bytedeco.javacv.*;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 
 import static org.bytedeco.javacpp.opencv_core.cvFlip;
+import static org.bytedeco.javacpp.opencv_core.cvSize;
+import static org.bytedeco.javacpp.opencv_videoio.CV_FOURCC;
+import static org.bytedeco.javacpp.opencv_videoio.cvWriteFrame;
 
 /**
  * Created by leon on 25.06.16.
@@ -22,9 +26,26 @@ opencv_core.IplImage img;
 BufferedImage temp;
 CanvasFrame cv;
 Rectangle face;
+boolean condition=true;
     public WebcamPanel() {
         cv=new CanvasFrame("Webcam");
         cv.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        cv.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                condition=false;
+            }
+            @Override
+            public void mousePressed(MouseEvent e) {}
+            @Override
+            public void mouseReleased(MouseEvent e) {}
+            @Override
+            public void mouseEntered(MouseEvent e) {}
+            @Override
+            public void mouseExited(MouseEvent e) {}});
+
+
+
         grabber=new OpenCVFrameGrabber("");
         face=new Rectangle(0,0,0,0);
         converter=new OpenCVFrameConverter.ToIplImage();
@@ -35,7 +56,6 @@ Rectangle face;
         catch (FrameGrabber.Exception e) {
             e.printStackTrace();
         }
-      //  this.add(cv);
     }
     public BufferedImage getImage()
     {
@@ -49,7 +69,7 @@ Rectangle face;
     }
     public void run() {
         int counter=0;
-      while(true){
+      while(condition){
        try {
             img = converter.convert(grabber.grab());
             if (img != null) {
